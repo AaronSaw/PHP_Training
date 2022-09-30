113b79ae4374375881c1836c906e3311b4020881
AaronSaw <aaronsaw702@gmail.com>
2022-09-30 17:10:11 +0630

    day 5

---
+++ tutorial-05/PhpWord/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0d42dde127976b11e572cf17cdf06e115fff6fa6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0d42dde127976b11e572cf17cdf06e115fff6fa6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'laminas/laminas-escaper' => array(
+            'pretty_version' => '2.10.0',
+            'version' => '2.10.0.0',
+            'reference' => '58af67282db37d24e584a837a94ee55b9c7552be',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laminas/laminas-escaper',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpoffice/phpword' => array(
+            'pretty_version' => '0.18.3',
+            'version' => '0.18.3.0',
+            'reference' => 'be0190cd5d8f95b4be08d5853b107aa4e352759a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpoffice/phpword',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ tutorial-05/index.php
@@ -0,0 +1,71 @@
+<?php
+echo "<h1>Reading Txt</h1>";
+$myfile = fopen("File/text.txt", "r");
+while (!feof($myfile)) {
+    echo fgets($myfile) . "<br>";
+}
+fclose($myfile); ?>
+<!--xlsx file -->
+<h1>Reading Xlsx</h1>
+<table border="1">
+    <?php
+    // (A) PHPSPREADSHEET TO LOAD EXCEL FILE
+    require "PhpSheet/vendor/autoload.php";
+    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+    $spreadsheet = $reader->load("File/test.xlsx");
+    $worksheet = $spreadsheet->getActiveSheet();
+    // (B) LOOP THROUGH ROWS OF CURRENT WORKSHEET
+    foreach ($worksheet->getRowIterator() as $row) {
+        // (B1) READ CELLS
+        $cellIterator = $row->getCellIterator();
+        $cellIterator->setIterateOnlyExistingCells(false);
+        // (B2) OUTPUT HTML
+        echo "<tr'>";
+        foreach ($cellIterator as $cell) {
+            echo "<td>" . $cell->getValue() . "</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
+<h1>Reading Csv</h1>
+<table border="1">
+    <?php
+    // (A) PHPSPREADSHEET TO LOAD EXCEL FILE
+    require "PhpSheet/vendor/autoload.php";
+    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+    $spreadsheet = $reader->load("File/csv_sample.csv");
+    $worksheet = $spreadsheet->getActiveSheet();
+    // (B) LOOP THROUGH ROWS OF CURRENT WORKSHEET
+    foreach ($worksheet->getRowIterator() as $row) {
+        // (B1) READ CELLS
+        $cellIterator = $row->getCellIterator();
+        $cellIterator->setIterateOnlyExistingCells(false);
+        // (B2) OUTPUT HTML
+        echo "<tr'>";
+        foreach ($cellIterator as $cell) {
+            echo "<td>" . $cell->getValue() . "</td>";
+        }
+        echo "</tr>";
+    }
+    ?></table>
+<h1>Reading Doc</h1>
+<?php
+require "PhpWord/vendor/autoload.php";
+//Reading From Document File
+$phpWord = \PhpOffice\PhpWord\IOFactory::load("File/saw.docx");
+echo "<br><br><h2>Reading from Document File!</h2>";
+$sections = $phpWord->getSections();
+foreach ($sections as $key => $value) {
+    $sectionElement = $value->getElements();
+    foreach ($sectionElement as $elementKey => $elementValue) {
+        if ($elementValue instanceof \PhpOffice\PhpWord\Element\TextRun) {
+            $secondSectionElement = $elementValue->getElements();
+            foreach ($secondSectionElement as $secondSectionElementKey => $secondSectionElementValue) {
+                if ($secondSectionElementValue instanceof \PhpOffice\PhpWord\Element\Text) {
+                    echo $secondSectionElementValue->getText();
+                }
+            }
+        }
+    }
+}
